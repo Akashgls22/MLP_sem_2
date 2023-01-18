c62b0723e30bd862e5fd645f09577b6fc917e42e
Akashgls22 <122084783+Akashgls22@users.noreply.github.com>
2023-01-18 14:52:34 +0530

    Add files via upload

---
+++ Pandas_day1 (1).py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+names=['Akash','Pratik','Jaymin','Trup']
+names
+
+
+# In[3]:
+
+
+marks=[40,50,60,60]
+marks
+
+
+# In[4]:
+
+
+type(marks)
+
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+df=pd.Series(data=marks)
+
+
+# In[7]:
+
+
+df
+
+
+# In[8]:
+
+
+type(df)
+
+
+# In[10]:
+
+
+cf=pd.Series(data=names)
+
+
+# In[11]:
+
+
+cf
+
+
+# In[12]:
+
+
+student={'names':names,'marks':marks}
+student
+
+
+# In[13]:
+
+
+type(student)
+
+
+# In[14]:
+
+
+df=pd.DataFrame(data=student)
+
+
+# In[15]:
+
+
+df
+
+
+# In[16]:
+
+
+df.shape
+
+
+# In[17]:
+
+
+df.info
+
+
+# In[18]:
+
+
+df.describe
+
+
+# In[19]:
+
+
+df.head()
+
+
+# In[20]:
+
+
+df.head(2)
+
+
+# In[21]:
+
+
+df.tail()
+
+
+# In[22]:
+
+
+df.tail(2)
+
+
+# In[26]:
+
+
+df=pd.read_csv('student1.csv')
+df
+
+
+# In[27]:
+
+
+df.head(2)
+
+
+# In[28]:
+
+
+df.columns
+
+
+# In[31]:
+
+
+df['Name']
+
+
+# In[32]:
+
+
+df.info
+
+
+# In[33]:
+
+
+df.describe
+
+
+# In[ ]:
+
+
+
+
